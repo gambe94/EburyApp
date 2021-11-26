d409018fe5fbdd6e197a6f147831db835424ba3b
Demeter Gábor <gdemeter@deloittece.com>
2021-11-26 12:32:37 +0100

    basic lwc and npm commands

--- force-app/main/default/lwc/listTrades/listTrades.js
+++ force-app/main/default/lwc/listTrades/listTrades.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
 
-export default class ListTrades extends LightningElement {}
+import getAllTrade from "@salesforce/apex/TradesController.getAllTrade";
+
+const columns = [
+  { label: "id", fieldName: "Name" },
+  { label: "Sell CCY", fieldName: "Sell_Currency__c" },
+  { label: "Sell Amount", fieldName: "Sell_Amount__c", type: "number" },
+  { label: "Buy CCY", fieldName: "Buy_Currency__c" },
+  { label: "Buy Amount", fieldName: "Buy_Amount__c", type: "number" },
+  { label: "Rate", fieldName: "Rate__c", type: "number" },
+  { label: "Date Booked", fieldName: "CreatedDate", type: "date" }
+];
+
+export default class ListTrades extends LightningElement {
+  data = [];
+  error;
+  columns = columns;
+
+  @wire(getAllTrade)
+  wiredContacts({ error, data }) {
+    if (data) {
+      this.data = data;
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.data = undefined;
+    }
+  }
+}
