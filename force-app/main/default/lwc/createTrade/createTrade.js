b40d6f3550dd9fc77b317527e775b602dc8105be
Demeter Gábor <gdemeter@deloittece.com>
2021-11-29 20:39:05 +0100

    Update UI part

--- force-app/main/default/lwc/createTrade/createTrade.js
+++ force-app/main/default/lwc/createTrade/createTrade.js
@@ -1,6 +1,7 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire, track } from "lwc";
+import { getPicklistValues, getObjectInfo } from "lightning/uiObjectInfoApi";
 
-// import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import doRateCallout from "@salesforce/apex/RateCalloutService.doCallout";
 
 import TRADE_OBJECT from "@salesforce/schema/Trade__c";
 import SELL_CCY from "@salesforce/schema/Trade__c.Sell_Currency__c";
@@ -9,8 +10,6 @@ import BUY__CCY from "@salesforce/schema/Trade__c.Buy_Currency__c";
 import BUY_AMOUNT from "@salesforce/schema/Trade__c.Buy_Amount__c";
 import RATE from "@salesforce/schema/Trade__c.Rate__c";
 
-const FETCH_RATE_URL = "http://data.fixer.io/api/latest";
-
 export default class CreateTrade extends LightningElement {
   tradeObject = TRADE_OBJECT;
   sellCcy = SELL_CCY;
@@ -18,6 +17,40 @@ export default class CreateTrade extends LightningElement {
   buyCcy = BUY__CCY;
   buyAmount = BUY_AMOUNT;
   rate = RATE;
+  rateVal;
+  sellAmountVal;
+  @track currencyList;
+
+  @wire(getObjectInfo, { objectApiName: TRADE_OBJECT })
+  wiredRecord({ error, data }) {
+    if (error) {
+      console.error(error);
+    } else if (data) {
+      console.log(data.recordTypeInfos);
+    }
+  }
+
+  get defaultRecordTypeId() {
+    // Returns a map of record type Ids
+    const rtis = this.objectInfo.data.recordTypeInfos;
+    return Object.keys(rtis).find((rti) => rtis[rti].name === "Master");
+  }
+
+  @wire(getPicklistValues, {
+    recordTypeId: "012000000000000AAA",
+    fieldApiName: SELL_CCY
+  })
+  wirePicklist({ error, data }) {
+    if (error) {
+      console.error(error);
+    } else if (data) {
+      console.log(data);
+      this.currencyList = data.values;
+    }
+  }
+  get calculatedBuyAmount() {
+    return this.rateVal * this.sellAmountVal;
+  }
 
   initCallout = {};
 
@@ -28,6 +61,9 @@ export default class CreateTrade extends LightningElement {
     console.log(event.target.fieldName);
     console.log(event.target);
     this.initCallout[event.target.fieldName] = event.target.value;
+    if (event.target.fieldName === "Sell_Amount__c") {
+      this.sellAmountVal = event.target.value;
+    }
     console.log(this.initCallout);
     this.doCallout();
   }
@@ -38,34 +74,23 @@ export default class CreateTrade extends LightningElement {
       this.initCallout.Sell_Amount__c &&
       this.initCallout.Buy_Currency__c
     ) {
-      let url =
-        FETCH_RATE_URL +
-        "?access_key=b2dd3bc469487ef1af9ad2aaea908c3b" +
-        "&base=" +
-        this.initCallout.Sell_Currency__c +
-        "&symbols=" +
-        this.initCallout.Buy_Currency__c;
-      console.log(url);
-
-      fetch(url, {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "include", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded'
-          "Access-Control-Allow-Origin": "http://data.fixer.io/"
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer-when-downgrade", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: null // body data type must match "Content-Type" header
+      doRateCallout({
+        isMock: true,
+        baseCurrency: this.initCallout.Sell_Currency__c,
+        targetCurrency: this.initCallout.Buy_Currency__c
       })
-        .then((response) => response.json()) // parses JSON response into native JavaScript objects
-        .then((data) => console.log(data))
-        .catch((error) => {
-          console.error(error);
-        });
+        .then((response) => {
+          let data = JSON.parse(response);
+          console.log(data);
+          console.log(this.sellAmountVal);
+          this.rateVal = data.rates[this.initCallout.Buy_Currency__c]; //data?.rates?.this.initCallout.Buy_Currency__c;
+        })
+        .catch((error) => console.error(error));
     }
+  }
+
+  handleClick(event) {
+    console.log(event.target.value);
+    console.log(this.sellAmountVal);
   }
 }
