9b42512c10002be54e0632373f7278c74ea7f774
Demeter Gábor <gdemeter@deloittece.com>
2021-12-07 17:58:39 +0100

    build script and make flow deactivated

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,23 @@ sfdx force:org:create -f config/project-scratch-def.json -a Dev --setdefaultuser
 
 sfdx force:source:push -u Dev
 
+#To give access to our system admin user
 sfdx force:user:permset:assign --permsetname Ebury_Trading
 
 # sfdx force:data:tree:export -q "SELECT Fixer_Api_Key__c FROM Integration_Configs__c" -d ./data
 #sfdx force:data:tree:export -q "SELECT  Id,  Name, Buy_Amount__c, Buy_Currency__c, Sell_Amount__c, Sell_Currency__c, Rate__c  FROM Trade__c" -d ./data
 
+#Import config records
 sfdx force:data:tree:import -f data/Integration_Configs__c.json
+#Import test trade records
 sfdx force:data:tree:import -f data/Trade__c.json
 
+#Create chatterGroup and add member
+sfdx force:apex:execute -f ./scripts/apex/createChatterGroup.apex 
 
+#Create test User
 sfdx force:user:create --setalias qa-user --definitionfile config/qa-userDef.json 
-sfdx force:user:display -u tester1@ebury.fake9999.org
+sfdx force:user:display -u qa-user
 
- sfdx force:source:deploy -p force-app/main/default/flows/Trade_Created.flow-meta.xml
+
+
