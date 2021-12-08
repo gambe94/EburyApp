688bf5736092763c592a72140f8143b929c8ce35
Demeter Gábor <gdemeter@deloittece.com>
2021-12-08 18:30:22 +0100

    add readme.md

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Ebury Trading App
 
 User easily can create Trade records in the Ebury Trading Salesforce Application.
-For conversion rate use flexi.io api.
+Use fixer.io api for rate currency conversion.
 
 ## Deployment
 
-There is a deployment script in place which you need to use from the root: ./scripts/build.sh
+There is a deployment script in place which you need to use from the root:
+
+```sh
+./scripts/build.sh
+```
+
+## Queue members insert
+
+There is a command which run anonymus apex to insert queue member into Trade reviewer.
+After pushing the metadata sometimes need to wait to be able to query the queue
+
+If command in script ,failed pls run this command again
+
+```sh
+sfdx force:apex:execute -f ./scripts/apex/addQueueMember.apex
+```
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #Create sratchOrg
-sfdx force:org:create -f config/project-scratch-def.json -a QAEbury --setdefaultusername
+sfdx force:org:create -f config/project-scratch-def.json -a QAEbury2 --setdefaultusername
 
 sfdx force:source:push
 
@@ -9,17 +9,20 @@ sfdx force:user:permset:assign --permsetname Ebury_Trading
 #Import test trade records
 sfdx force:data:tree:import -f data/Trade__c.json
 
+
+#Create test User
+sfdx force:user:create --setalias qa-user --definitionfile config/qa-userDef.json
+
+#After metadatapush we some times tu run query to retrieve the queue
 #Add queue members
 sfdx force:apex:execute -f ./scripts/apex/addQueueMember.apex 
 
 #Init customsetting
 sfdx force:apex:execute -f ./scripts/apex/initCustomSetting.apex 
 
-#Create test User
-sfdx force:user:create --setalias qa-user --definitionfile config/qa-userDef.json 
-
 #Display Login info for testing user
 sfdx force:user:display -u qa-user
 
-
+#Open the org as qa-user
+sfdx force:org:open -u qa-user
 
